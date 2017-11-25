aa2c7608a4847d1254c44b397378750552beabae
NAzarMh <nazar.myhailiv@gmail.com>
2017-11-25 23:20:38 +0200

    revert

--- src/lesson11/lesson_11_1.java
+++ src/lesson11/lesson_11_1.java
@@ -2,8 +2,6 @@ package lesson11;
 
 public class lesson_11_1 {
     public static void main(String[] args) {
-
-        System.out.println("test github") ;
-        System.out.println("New test line");
+        System.out.println("test github");
     }
 }
