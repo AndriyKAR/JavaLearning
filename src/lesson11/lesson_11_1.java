7bc000dcae728f51e857bb76c99b8ec28b264ca1
AndriyKar <akarachevskyy@ukr.net>
2017-11-25 23:16:32 +0200

    change string

--- src/lesson11/lesson_11_1.java
+++ src/lesson11/lesson_11_1.java
@@ -2,6 +2,8 @@ package lesson11;
 
 public class lesson_11_1 {
     public static void main(String[] args) {
-        System.out.println("test github");
+
+        System.out.println("test github") ;
+        System.out.println("New test line");
     }
 }
